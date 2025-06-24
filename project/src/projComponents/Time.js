35d54369618ec4763b545c255b0ad74d2cdd943f
mohammed alsamaki <samkimohd@gmail.com>
2025-06-24 15:55:56 +0300

    connect backend with front  add profile

--- project/src/projComponents/Time.js
+++ project/src/projComponents/Time.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import dayjs from 'dayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -7,21 +7,20 @@ import { TimeClock } from '@mui/x-date-pickers/TimeClock';
 import Button from '@mui/material/Button';
 
 export default function TimeClockValue() {
-  const [value, setValue] = React.useState(dayjs('2022-04-17T00:00'));
+  const [value, setValue] = React.useState(dayjs());
+  const [clockValue, setClockValue] = React.useState(dayjs());
 
-  const[clockvalue,setCklockValue]=React.useState(dayjs('2022-04-17T00:00'))
-  const handleClockValue=()=>{
-    setCklockValue(value)
-  }
+  const handleClockValue = () => {
+    setClockValue(value);
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['TimeClock', 'TimeClock']}>
-        
+      <DemoContainer components={['TimeClock']}>
         <DemoItem label="Controlled clock">
           <TimeClock value={value} onChange={(newValue) => setValue(newValue)} />
         </DemoItem>
-        <p> {clockvalue.format('HH:mm')}</p>
+        <p>{clockValue.format('HH:mm')}</p>
         <Button color='success' onClick={handleClockValue}>Start</Button>
       </DemoContainer>
     </LocalizationProvider>
--- project/src/projComponents/progressBar.js
+++ project/src/projComponents/progressBar.js
@@ -1,38 +1,28 @@
-import React from "react";
-import { useState,useContext, useRef} from "react";
-import '../projStyle/progressBar.css'
+import React, { useContext, useRef } from "react";
+import '../projStyle/progressBar.css';
 import Button from "@mui/material/Button";
 import ChartContext from "./chartContext";
 
-export default function Progress(){
-    
- const {checkedValue} = useContext(ChartContext)
- console.log(checkedValue)
-
-const [progress, setProgress] = useState(20);
+export default function Progress() {
+  const { checkedValue } = useContext(ChartContext);
   const intervalRef = useRef(null);
 
   const move = () => {
-    if (intervalRef.current) return; // prevent multiple intervals
-
+    if (intervalRef.current) return;
     intervalRef.current = setInterval(() => {
-      setProgress(prev => {
-        if (prev >= 100) {
-          clearInterval(intervalRef.current);
-          intervalRef.current = null;
-          return 100;
-        }
-        return prev + 1;
-      });
+      if (checkedValue >= 100) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
     }, 100);
   };
 
-    return(
-        <div>
-            <div className="prog-container">
-                <div id="bar" className="prog-green" style={{ width: `${checkedValue}%` }}>{checkedValue}%</div>
-            </div>
-            <Button>progress</Button>
-        </div>
-    )
+  return (
+    <div>
+      <div className="prog-container">
+        <div className="prog-green" style={{ width: `${checkedValue}%` }}>{checkedValue}%</div>
+      </div>
+      <Button onClick={move}>progress</Button>
+    </div>
+  );
 }
